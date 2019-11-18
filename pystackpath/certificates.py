7b74ac8dde612282327bb7c545f9ab8b8ef807eb
Sandro Modarelli <sandro.modarelli@namecheap.com>
2019-11-18 11:37:40 +0100

    handled url construction during inheritance

--- pystackpath/certificates.py
+++ pystackpath/certificates.py
@@ -4,7 +4,7 @@ from .util import BaseObject, PageInfo, pagination_query
 class Certificates(BaseObject):
         def index(self, first="", after="", filter="", sort_by=""):
             pagination = pagination_query(first=first, after=after, filter=filter, sort_by=sort_by)
-            response = self._client.get(f"/cdn/v1/stacks/{self._parent_id}/certificates", params=pagination)
+            response = self._client.get(f"{self._base_api}/certificates", params=pagination)
             response.raise_for_status()
 
             items = list(map(lambda x: self.loaddict(x), response.json()["results"]))
@@ -13,7 +13,7 @@ class Certificates(BaseObject):
             return {"results": items, "pageinfo": pageinfo}
 
         def get(self, certificate_id: str):
-            response = self._client.get(f"/cdn/v1/stacks/{self._parent_id}/certificates/{certificate_id}")
+            response = self._client.get(f"{self._base_api}/certificates/{certificate_id}")
             response.raise_for_status()
 
             return self.loaddict(response.json()["certificate"])
@@ -25,13 +25,13 @@ class Certificates(BaseObject):
                 "caBundle" : ca_bundle_string
             }
 
-            response = self._client.post(f"/cdn/v1/stacks/{self._parent_id}/certificates", json=data)
+            response = self._client.post(f"{self._base_api}/certificates", json=data)
             response.raise_for_status()
 
             return self.loaddict(response.json()["certificate"])
 
         def delete(self):
-            response = self._client.delete(f"/cdn/v1/stacks/{self._parent_id}/certificates/{self.id}")
+            response = self._client.delete(f"{self._base_api}/certificates/{self.id}")
             response.raise_for_status()
 
             return self
@@ -43,13 +43,13 @@ class Certificates(BaseObject):
                 "caBundle" : ca_bundle_string
             }
 
-            response = self._client.put(f"/cdn/v1/stacks/{self._parent_id}/certificates/{self.id}", json=data)
+            response = self._client.put(f"{self._base_api}/certificates/{self.id}", json=data)
             response.raise_for_status()
 
             return self.loaddict(response.json()["certificate"])
 
         def renew(self):
-            response = self._client.post(f"/cdn/v1/stacks/{self._parent_id}/certificates/{self.id}/renew")
+            response = self._client.post(f"{self._base_api}/certificates/{self.id}/renew")
             response.raise_for_status()
 
             return self
