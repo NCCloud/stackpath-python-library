7ceaaa7b2764b3e9233229af741b46429f9e028d
Sandro Modarelli <sandro.modarelli@namecheap.com>
2019-11-18 09:28:35 +0100

    delivery apis interaction

--- pystackpath/deliverysites.py
+++ pystackpath/deliverysites.py
@@ -1,20 +1,29 @@
-from .deliverysites import DeliverySites
-from .util import PageInfo, pagination_query
+from .util import BaseObject, PageInfo, pagination_query
 
 
-class CdnSites(DeliverySites):
-    base_api = "/cdn"
+class DeliverySites(BaseObject):
+    base_api = "/delivery"
 
     def index(self, first="", after="", filter="", sort_by=""):
-        return super().index(first="", after="", filter="", sort_by="")
+        pagination = pagination_query(first=first, after=after, filter=filter, sort_by=sort_by)
+        response = self._client.get("{}/v1/stacks/{}/sites".format(self.base_api, self._parent_id), params=pagination)
+        response.raise_for_status()
+        items = []
+        for item in response.json()["results"]:
+            items.append(self.loaddict(item))
+        pageinfo = PageInfo(**response.json()["pageInfo"])
+
+        return {"results": items, "pageinfo": pageinfo}
 
     def get(self, site_id):
-        return super().get(site_id)
+        response = self._client.get("{}/v1/stacks/{}/sites/{}".format(self.base_api, self._parent_id, site_id))
+        response.raise_for_status()
+        return self.loaddict(response.json()["site"])
 
     def create(self, **payload):
         """
-        Create a new CDN site
-        :param payload: dict according to https://stackpath.dev/reference/sites-2#createsite
+        Create a new site
+        :param payload: dict according to https://stackpath.dev/reference/sites#createsite-1
         :return: dict with created site
         String	id         A CDN site's unique identifier.
         String	stackId    The ID of the stack to which a CDN site belongs.
@@ -25,23 +34,22 @@ class CdnSites(DeliverySites):
         String	updatedAt  The date that a CDN site was last updated.
         List	features   A CDN site's associated features.
                            Features control how StackPath provisions and configures a site.
-        Boolean	enabled    Whether or not a site's CDN service is enabled.
-        String	type       A CDN site's type.
-                           A site's type determines how StackPath delivers content to incoming HTTP(S) requests.
-                           UNKNOWN: StackPath is unable to determine a site's type
-                           CDN: A site is CDN only site
-                           WAF: A site is either a standalone WAF site or a WAF site with attached CDN service
-                           API: A site is an API delivery site. API delivery sites are powered by both the WAF and CDN
-                                and have custom rulesets for each.
         """
-        return super().create(**payload)
+        response = self._client.post(
+            "{}/v1/stacks/{}/sites".format(self.base_api, self._parent_id),
+            json=payload
+        )
+        response.raise_for_status()
+        return self.loaddict(response.json()["site"])
 
     def delete(self):
         """
         Delete a CDN site
         :return: a stackpath site object with the deleted cdn site
         """
-        return super().delete()
+        response = self._client.delete("{}/v1/stacks/{}/sites/{}".format(self.base_api, self._parent_id, self.id))
+        response.raise_for_status()
+        return self
 
     def disable(self):
         """
